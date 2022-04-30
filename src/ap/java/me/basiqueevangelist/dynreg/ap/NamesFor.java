900cf046fe2b337b55216c4a89a85a83242d6848
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-04-30 11:01:11 +0300

    use annotation processor for NamedEntries

---
+++ src/ap/java/me/basiqueevangelist/dynreg/ap/NamesFor.java
@@ -0,0 +1,12 @@
+package me.basiqueevangelist.dynreg.ap;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NamesFor {
+    Class<?> value();
+}
